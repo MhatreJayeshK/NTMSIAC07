93c72ce959bc180a1d4386782e30dea50fd5d094
MhatreJayeshK <51264708+MhatreJayeshK@users.noreply.github.com>
2022-01-15 13:05:44 +0530

    added new RG3

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+ 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=2.91.0"
     }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "JayeshMhatre-NTMSIAC07"
+  name     = "JayeshMhatre2"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "Jayesh-RG" {
+  name     = "JayeshMhatre3"
   location = "West Europe"
 }
